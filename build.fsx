8d1a12e73846979b195864819f2b40f71d095644
Sandor Rakonczai <rakonczai.sandor@outlook.hu>
2016-01-28 18:27:14 +0100

    Add build scripts

---
+++ build.fsx
@@ -0,0 +1,49 @@
+#load "tools/includes.fsx"
+
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("WebSharper.Peity")
+        .VersionFrom("WebSharper")
+
+let extensions =
+    bt.WebSharper.Extension("WebSharper.Peity.Bindings")
+        .SourcesFromProject()
+        .Embed(["jquery.peity.min.js"])
+
+let wrapper =
+    bt.WebSharper.Library("WebSharper.Peity")
+        .SourcesFromProject()
+        .References(fun ref -> 
+            [
+                ref.Project extensions
+            ])
+
+let tests =
+    bt.WebSharper.BundleWebsite("WebSharper.Peity.Tests")
+        .SourcesFromProject()
+        .References(fun ref -> 
+            [
+                ref.Project extensions
+                ref.Project wrapper
+                (ref.NuGet "WebSharper.Html").Reference()
+            ])
+
+bt.Solution [
+    extensions
+    wrapper
+    tests
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun configuration ->
+            { configuration with
+                Title = Some "WebSharper.Peity"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.react"
+                Description = "WebSharper bindings for Peity (http://benpickles.github.io/peity/)."
+                Authors = [ "IntelliFactory" ]
+                RequiresLicenseAcceptance = true })
+        .Add(extensions)
+        .Add(wrapper)
+]
+|> bt.Dispatch
