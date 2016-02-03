5d943d7b301e6297ca6abad73a2c8f2afe637527
Sandor Rakonczai <rakonczai.sandor@outlook.hu>
2016-02-03 17:50:14 +0100

    Add reactive chart-type

--- WebSharper.Peity.Tests/Client.fs
+++ WebSharper.Peity.Tests/Client.fs
@@ -4,12 +4,12 @@ open WebSharper
 open WebSharper.JavaScript
 open WebSharper.JQuery
 open WebSharper.Peity
-open WebSharper.Html.Client
 
 [<JavaScript>]
-module Client =
-    
-    let Main =
+module Test0 =
+    open WebSharper.Html.Client
+
+    let chart =
         Span [
             [|0 .. 10|]
             |> Array.map (fun _ ->
@@ -22,6 +22,27 @@ module Client =
             JQuery.Of(data.Dom).Peity("line", PeityConfig(Width = 64))
             |> ignore
         )
-        |> (fun node ->
-            node.AppendTo "body"
-        )
+
+[<JavaScript>]
+module Test1 =
+    
+    let chart =
+        UpdatingChart([], PeityConfig(Width = 64))
+
+[<JavaScript>]
+module Client =
+    open WebSharper.UI.Next.Client
+    open WebSharper.UI.Next.Html
+
+    let main =
+        (Html.Client.Tags.Div [ Test0.chart ]).AppendTo "body"
+        
+        async {
+            for _ in 1 .. 10 do
+                do! Async.Sleep 500
+                (Test1.chart.AddValue << double) <| Math.Floor(Math.Random() * 10.0)
+        }
+        |> Async.Start
+
+        div [ Test1.chart.Doc ]
+        |> Doc.RunAppendById "body"
--- WebSharper.Peity/Main.fs
+++ WebSharper.Peity/Main.fs
@@ -3,9 +3,12 @@ namespace WebSharper.Peity
 open WebSharper
 open WebSharper.JavaScript
 open WebSharper.JQuery
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
 
 [<AutoOpen>]
-module Extension =
+module JQueryExtensions =
 
     type JQuery with
         [<Inline "$0.peity($t)">]
@@ -13,3 +16,43 @@ module Extension =
 
         [<Inline "$0.peity($t, $config)">]
         member x.Peity(t: string, config: PeityConfig) = X<JQuery>
+
+[<JavaScript>]
+module Option =
+    
+    let toObj (option: 'T option) =
+        match option with
+        | Some o -> o
+        | _      -> null
+
+[<JavaScript>]
+module Seq =
+    
+    let inline indexed (source: seq<'T>) = Seq.mapi (fun i v -> (i, v)) source
+
+[<JavaScript>]
+type UpdatingChart(data: seq<double>, ?config: PeityConfig) =
+    let data = ListModel.Create fst (Seq.indexed data)
+    
+    member val Doc =
+        data.View
+        |> Doc.BindView (fun s ->
+            spanAttr [
+                on.afterRender (fun e ->
+                    JQuery.Of(e).Peity("line", (Option.toObj config)) |> ignore
+                )
+            ] [
+                let delimiter = 
+                    match config with
+                    | Some c -> c.Delimiter
+                    | None   -> ","
+                
+                yield Seq.map (fun (_, v) -> string v) s
+                    |> String.concat delimiter
+                    |> text
+            ]
+        ) with get
+
+    member this.AddValue v = data.Add (data.Length, v)
+
+    member this.Update s = data.Set (Seq.indexed s)
